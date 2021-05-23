68b5e1a238fdee244b593f0dd5f6d1a6724cb7b7
Harold Torres Marino <haroldtm55@gmail.com>
2021-05-23 23:17:42 +1000

    Tests passed

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books').then(response => response.json()).then(json => {
+    console.log(json[4])
+    const totalNumberOfPages = json.map((object) => object
+    ['numberOfPages']).reduce((accumulator,currentValue) => accumulator + currentValue)
+    console.log(`There are a total of ${totalNumberOfPages} pages`)
+    let characters = json.map((object) => object['characters'])
+    let charactersAllTogether = characters[0]
+    for (let i = 1; i <= characters.length -1; i++) {
+      charactersAllTogether = charactersAllTogether.concat(characters[i])
+    }
+    let character1031 = charactersAllTogether.find((string)=> string === 'https://anapioficeandfire.com/api/characters/1031')
+    console.log(`The info about the 1031st character is in the following link: ${character1031}`)
+    renderBooks(json)
+  })
 }
 
 function renderBooks(books) {
